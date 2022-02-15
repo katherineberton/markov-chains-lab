c628fe6f5d160e30b6ae4e5366846f7e8ebc7226
Kat Berton <katherine.berton@gmail.com>
2022-02-15 12:58:49 -0800

    working Markov chain generator

--- markov.py
+++ markov.py
@@ -10,9 +10,9 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
-
-    return 'Contents of your file as one long string'
+    contents = open(file_path).read()
+    
+    return contents
 
 
 def make_chains(text_string):
@@ -41,8 +41,46 @@ def make_chains(text_string):
     """
 
     chains = {}
+    #               create the list of single words from the file
 
-    # your code goes here
+
+    #rstrip the file's text and replace line breaks with spaces for later use of the .split(" ") function
+    formatted = text_string.rstrip().replace("\n", " ")
+    
+    #then use .split(" ") to turn it into a list
+    list_of_words = formatted.split(" ")
+    
+    #                populate the dictionary
+    
+
+    #for loop over a range of the list to create dict keys, range (not inclusive) to accommodate the final tuple which starts at index len(list_of_words) - 2
+    for i in range(0,(len(list_of_words)-1)):
+
+        #create variable named tup equal to (word at index i, following word)
+        tup = (list_of_words[i], list_of_words[i+1])
+
+        #if it is the last possible tuple in the list:
+        if i == len(list_of_words) - 2:
+
+            #assign the value to a list containing empty string
+            chains[tup] = [""]
+
+        #otherwise, if it's any other tuple than the last one
+        else:
+
+            #if the tuple is already a key in the dictionary:
+
+            if tup in chains:
+
+                #append the value list with the word following the tuple
+                chains[tup].append(list_of_words[i+2])
+
+            #otherwise, if it's not already in the list
+            else:
+
+                #create new key value pair with tuple: a list containing the following word as its only element
+                chains[tup] = [list_of_words[i+2]]
+    
 
     return chains
 
@@ -52,7 +90,23 @@ def make_text(chains):
 
     words = []
 
-    # your code goes here
+
+    #while loop until error saying last tuple of (last word, "") doesn't exist (whatever that looks like not sure right now)
+
+    initial_key = choice(list(chains))
+    words.append(initial_key[0]) #add first thing in key to words list
+    words.append(initial_key[1]) #add second thing in key to words list
+
+    current_key = initial_key
+
+    #leave the loop when we have reached the end of the text, or in other words, when the value of the tuple is ""
+    while chains[current_key] != [""]:
+
+        new_value = choice(chains[current_key]) #choose random value from current key
+
+        words.append(new_value) #append list with retrieved value
+
+        current_key = (current_key[1], new_value) #update current key to drop first item and add the previously added value
 
     return ' '.join(words)
 
